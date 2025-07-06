b1ade37dc6c35b899505f68acc2c743c2e95c932
delower.hosen <delower.hosen00@gmail.com>
2025-07-06 22:53:42 +0600

    migration added

--- EFCoreDemo/WebService/Program.cs
+++ EFCoreDemo/WebService/Program.cs
@@ -40,6 +40,10 @@ if (app.Environment.IsDevelopment() || enableSwagger)
 
 }
 
+using var scope = app.Services.CreateScope();
+var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+db.Database.Migrate();
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
