c6599fedb269b963e96310e59e81ff51e0c54a4b
delower.hosen <delower.hosen00@gmail.com>
2025-07-11 15:24:57 +0600

    solution name changed

---
+++ src/Domain/CommandHandlers/CreateProductCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Commands;
+using Domain.Contracts;
+using MediatR;
+
+namespace Domain.CommandHandlers
+{
+    public class CreateProductCommandHandler(ICreateProductCommandService createProductCommandService) : IRequestHandler<CreateProductCommand, bool>
+    {
+        private readonly ICreateProductCommandService _createProductCommandService = createProductCommandService;
+
+        public async Task<bool> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            await _createProductCommandService.CreateProductAsync(command);
+            return true;
+        }
+    }
+}
---
+++ src/WebService/Program.cs
@@ -0,0 +1,53 @@
+using Domain.CommandHandlers;
+using EfCore.PostgreSql;
+using EfCore.PostgreSql.Repositories;
+using Microsoft.EntityFrameworkCore;
+using ServiceCollector;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(typeof(CreateProductCommandHandler).Assembly);
+});
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+builder.Services.AddProductCommandServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+var enableSwagger = builder.Configuration.GetValue<bool>("EnableSwaggerInProd");
+
+if (app.Environment.IsDevelopment() || enableSwagger)
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+
+}
+
+using var scope = app.Services.CreateScope();
+var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+db.Database.Migrate();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
