7f8b2d4b29423ba534ceb6536ff859a28c2eebe5
delower.hosen <delower.hosen00@gmail.com>
2025-07-12 13:10:28 +0600

    service health check

---
+++ src/WebService/Controllers/PingController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebService.Controllers
+{
+    public class PingController : ApiControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("Healthy");
+    }
+}
